981e2e96707de5b57a32622195973839eb94924f
Mr. Baier <paulbaier@hunschool.org>
2026-01-10 13:51:37 -0500

    select all from actor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,31 @@
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         String url = "jdbc:sqlite:/Users/paulbaier/School/SoftwareEngineering/db/Sakila";
+        Connection conn = null;
+        PreparedStatement sql = null;
         try {
-            DriverManager.getConnection(url);
+            conn = DriverManager.getConnection(url);
             System.out.println("Connected to SQLite!");
+            sql = conn.prepareStatement("SELECT * FROM actor");
+            ResultSet rs = null;
+            try {
+                rs = sql.executeQuery();
+                while (rs.next()) {
+                    int id = rs.getInt("actor_id");
+                    String firstName = rs.getString("first_name");
+                    String lastName = rs.getString("last_name");
+                    System.out.println("%d: %s %s".formatted(id, firstName, lastName));
+                }
+            } finally {
+                if (rs != null) rs.close();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (sql != null) sql.close();
+            if (conn != null) conn.close();
         }
     }
 }
